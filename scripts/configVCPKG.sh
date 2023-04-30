b94eaf15490265bc19034213d0ffc3771e201ecf
achref-abidi <achref.abidi@ensi-uma.tn>
2023-04-30 18:49:44 +0100

    fix errors

--- scripts/configVCPKG.sh
+++ scripts/configVCPKG.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# Get the installation path from the user
-read -p "Enter the installation path: " install_path
+# Set the installation path
+install_path=~/vcpkg
 
 # Clone the Git repository
-git clone git clone https://github.com/microsoft/vcpkg $install_path/vcpkg
-
-# Change to the installation directory
-cd $install_path
+git clone git clone https://github.com/microsoft/vcpkg $install_path
 
 # Bootstapping vcpkg
-./bootstrap-vcpkg.sh
+$install_path/bootstrap-vcpkg.sh
 
 # Add the installation directory to the user's PATH
 echo "export PATH=\$PATH:$install_path" >> ~/.bashrc
--- scripts/envSetup.sh
+++ scripts/envSetup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+apt-get update
+apt-get cmake make 
+
 cd ..
 git submodule update --init --recursive
