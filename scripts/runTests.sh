f00823409b42a1668fc55876f460b76d1778e214
achref-abidi <achref.abidi@ensi-uma.tn>
2023-04-30 23:14:03 +0100

    added tests template

---
+++ scripts/runTests.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd ..
+ctest --preset="test-alDbg"
---
+++ tests/TestCommands.cpp
@@ -0,0 +1,11 @@
+//
+// Created by achref abidi on 30 April 2023
+//
+#include <gtest/gtest.h>
+#include <iostream>
+
+int main()
+{
+    std::cout << "hello world" << std::endl;
+    return 0;
+}
